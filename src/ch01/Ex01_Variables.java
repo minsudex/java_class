2c5948c80d7d520f8452a78b1acb91347d7a4cad
minsudex <minsuf4@naver.com>
2023-11-30 14:07:47 +0900

    addres

---
+++ src/ch01/Ex01_Variables.java
@@ -0,0 +1,10 @@
+package ch01;
+
+public class Ex01_Variables {
+
+	public static void main(String[] args) {
+	String name = "김민수";
+	int age = 25;
+	double height = 170;
+	String address = "인천시 남동구 만수동";
+	String mobile = "010-4063-7036";
