4003258b2e70fc12e0157dcffe78daeb99ca3918
minsudex <minsuf4@naver.com>
2023-12-05 13:28:21 +0900

    Ex08 구구단

---
+++ src/ch06_for/Ex08_ForEx.java
@@ -0,0 +1,26 @@
+package ch06_for;
+
+import java.util.concurrent.LinkedTransferQueue;
+
+public class Ex08_ForEx {
+
+    /***
+     * manin method
+     * @param args
+     */
+    public static void main(String[] args) {
+        /**
+         * 구구단 출력하기
+         */
+        for (int i =2; i<=9; i++) { // 몇 단
+            System.out.println(i+"단");
+            for (int j =1; j <= 9; j++) {   // 곱하는 수(1~9)
+                System.out.print(i + "x" + j + "=" + (i+j) + " ");
+                System.out.print("");
+            }
+            System.out.println();
+            System.out.println("");
+
+        }
+    }
+}
