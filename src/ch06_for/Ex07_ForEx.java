4d5e02dd98df4ff409e9842a38aca7c59d796d25
minsudex <minsuf4@naver.com>
2023-12-05 11:22:38 +0900

    Ex_07(바깥,안 For문)

---
+++ src/ch06_for/Ex07_ForEx.java
@@ -0,0 +1,21 @@
+package ch06_for;
+
+import com.sun.security.jgss.GSSUtil;
+
+public class Ex07_ForEx {
+    public static void main(String[] args) {
+        /**
+         * 중첩 for문
+         */
+        System.out.println("바깥 for문 시작");
+        for (int i = 1; i <= 2; i++) {
+            System.out.println("안족 for문 시작");
+            for (int j = 1; j <= 3; j++) {
+            System.out.print("j = " + j);
+            System.out.println("    i = " +  i);
+        }
+        System.out.println("안쪽 for문 종료");
+    }
+        System.out.println("바깥 for문 종료");
+}
+}
