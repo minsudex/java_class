9b3d2466827399a573ed8b1f15cbda1b46d67d66
minsudex <minsuf4@naver.com>
2023-12-06 09:22:11 +0900

    while문 시작

--- src/ch06_for/Ex11_ForEX.java
+++ src/ch06_for/Ex11_ForEX.java
@@ -14,16 +14,19 @@ public class Ex11_ForEX {
          */
 
         // 행마다 별을 찍는 for문
-        for (int i = 0; i < 5 ; i++) {
+        for (int i = 0; i < 5; i++) {  // 줄수, 별을 출력할 횟수
             // 열의 개수만큼 별을 찍는 for문
-            for (int j = 0; j <= i; j++) {
+            for (int j = 0; j <= i; j++) {  // 별을 줄수만큼 출력
                 System.out.print("*");
             }
             // 줄바꿈
             System.out.println();
+
+
         }
     }
 }
+
 
 
 
