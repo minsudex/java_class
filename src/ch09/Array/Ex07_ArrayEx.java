ffc984b52cdf615956889980b9217c24282d2486
minsudex <minsuf4@naver.com>
2023-12-08 14:33:51 +0900

    오름차순 정렬

---
+++ src/ch09/Array/Ex07_ArrayEx.java
@@ -0,0 +1,32 @@
+package ch09.Array;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Ex07_ArrayEx {
+    public static void main(String[] args) {
+        /** int[] = {3,2,4,1,5}
+         * 위와 같은 배열이 있을 때
+         * {1, 2, 3, 4, 5} 와 같이 오름차순 정렬을
+         * 하는 코드를 작성
+         */
+        int[] num = {3, 2, 4, 1, 5};
+
+        //  중첩 for를 이용하여 정렬하기
+        for (int i = 0; i < num.length - 1; i++) {
+            for (int j = i + 1; j < num.length; j++) {
+                if (num[i] > num[j]) {
+                    int temp = num[i];
+                    num[i] = num[j];
+                    num[j] = temp;
+                }
+            }
+        }
+
+        // 정렬된 배열을 출력
+        for (int i = 0; i < num.length; i++) {
+            System.out.println(num[i]);
+        }
+    }
+}
+
