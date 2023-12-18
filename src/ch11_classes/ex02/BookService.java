116afb863dfb23fe7f7f8b98efbe6251cb2dea36
minsudex <minsuf4@naver.com>
2023-12-18 09:40:17 +0900

    도서관리예제 - 도서 삭제 기능

--- src/ch11_classes/ex02/BookService.java
+++ src/ch11_classes/ex02/BookService.java
@@ -1,14 +1,11 @@
 package ch11_classes.ex02;
 
-import sun.security.krb5.internal.ccache.MemoryCredentialsCache;
-
 import java.util.List;
 import java.util.Scanner;
 
 public class BookService {
     Scanner scanner = new Scanner(System.in);
     BookRepository bookRepository = new BookRepository();
-
 
     /**
      * 도서등록 메서드
@@ -20,15 +17,14 @@ public class BookService {
      * BookDTO 객체에 담아서 Repository로 전달하여 저장이 되도록 함
      * 등록 여부를 출력한다.(등록성공 or 등록실패)
      */
-    /* 도서등록 메서드  */
     public void save() {
-        System.out.println("도서명; AI 이후의 세계");
+        System.out.print("도서명: AI 이후의 세계 ");
         String bookTitle = scanner.next();
-        System.out.println("저자; 헨리 키신저, 에릭 슈미트, 대니얼 허튼로커, 김대식");
+        System.out.print("저자: 헨리 키신저, 에릭 슈밋, 대니얼 허톤로커, 김대식 ");
         String bookAuthor = scanner.next();
-        System.out.println("가격;17820원 ");
-        String bookPrice = scanner.next();
-        System.out.println("출판사 정보; 월북");
+        System.out.print("가격: 17820");
+        int bookPrice = scanner.nextInt();
+        System.out.print("출판사: 윌북");
         String bookPublisher = scanner.next();
         BookDTO bookDTO = new BookDTO(bookTitle, bookAuthor, bookPrice, bookPublisher);
         boolean result = bookRepository.save(bookDTO);
@@ -38,6 +34,7 @@ public class BookService {
             System.out.println("등록 실패");
         }
     }
+
 
     /**
      * 도서목록 메서드
@@ -49,7 +46,7 @@ public class BookService {
      */
     public void findAll() {
         List<BookDTO> bookDTOList = bookRepository.findAll();
-        for (BookDTO bookDTO : bookDTOList) {
+        for (BookDTO bookDTO: bookDTOList) {
             System.out.println("bookDTO = " + bookDTO);
         }
     }
@@ -64,16 +61,6 @@ public class BookService {
      *      id를 입력받고 Repository로 부터 id에 해당 하는 도서 정보를 출력
      *      없으면 없다고 출력
      */
-
-    /**
-     * 도서조회 메서드
-     * name: findByTitle
-     * parameter: x
-     * return: x
-     * 실행내용
-     * 도서제목을 입력받고 Repository로 부터 해당 하는 도서 정보를 출력(제목이 정확히 일치하는 것만)
-     * 없으면 없다고 출력
-     */
     public void findById() {
         System.out.print("조회 id: ");
         Long id = scanner.nextLong();
@@ -82,6 +69,73 @@ public class BookService {
             System.out.println("bookDTO = " + bookDTO);
         } else {
             System.out.println("조회결과가 없습니다!");
+        }
+    }
+
+
+    /**
+     * 도서조회 메서드
+     * name: findByTitle
+     * parameter: x
+     * return: x
+     * 실행내용
+     *      도서제목을 입력받고 Repository로 부터 해당 하는 도서 정보를 출력(제목이 정확히 일치하는 것만)
+     *      없으면 없다고 출력
+     */
+    public void findByTitle() {
+        System.out.print("도서명: ");
+        String bookTitle = scanner.next();
+        BookDTO bookDTO = bookRepository.findByTitle(bookTitle);
+        if (bookDTO != null) {
+            System.out.println("bookDTO = " + bookDTO);
+        } else {
+            System.out.println("조회결과가 없습니다!");
+        }
+    }
+
+    public void search() {
+        System.out.print("검색어: ");
+        String bookTitle = scanner.next();
+        List<BookDTO> bookDTOList = bookRepository.search(bookTitle);
+        if (bookDTOList.size() > 0) {
+            for (BookDTO bookDTO: bookDTOList) {
+                System.out.println("bookDTO = " + bookDTO);
+            }
+        } else {
+            // bookDTOList.size() == 0 => 결과가 없다
+            System.out.println("검색 결과가 없습니다!");
+        }
+    }
+
+    public void update() {
+        // 수정할 id를 입력받음
+        // 해당 id 도서가 있다면 수정할 가격을 입력받고 수정 처리
+        // 없으면 없다고 출력
+        System.out.print("수정할 id: ");
+        Long id = scanner.nextLong();
+        BookDTO bookDTO = bookRepository.findById(id);
+        if (bookDTO != null) {
+            System.out.print("수정할 가격: ");
+            int bookPrice = scanner.nextInt();
+            boolean updateResult = bookRepository.update(id, bookPrice);
+            if (updateResult) {
+                System.out.println("수정 성공");
+            } else {
+                System.out.println("수정 실패");
+            }
+        } else {
+            System.out.println("조회결과가 없습니다!");
+        }
+    }
+
+    public void delete() {
+        System.out.print("삭제할 id: ");
+        Long id = scanner.nextLong();
+        boolean result = bookRepository.delete(id);
+        if (result) {
+            System.out.println("삭제 성공");
+        } else {
+            System.out.println("삭제 실패");
         }
     }
 }
