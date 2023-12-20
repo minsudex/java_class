680d5f6ee5d2b027f4b769920f5b3c06c65dd3a0
minsudex <minsuf4@naver.com>
2023-12-20 13:38:32 +0900

    게시판 프로젝트 - 테스트데이터 전용 메서드 추가 * 주석 추가

--- src/ch10_class/ex11/BoardMain.java
+++ src/ch10_class/ex11/BoardMain.java
@@ -14,33 +14,33 @@ public class BoardMain {
             끝번호를 입력받고 Board 클래스의 findById로 글번호를 넘긴다.
          * 3. 종료
          */
-        Scanner scanner = new Scanner(System.in);   /*  Scanner 클래스를 사용하여 키보드 입력을 받습니다. */
-        boolean run = true; /*  run 변수를 사용하여 프로그램이 실행 중인지 여부를 제어합니다.    */
-        int selectNo = 0;   /*  selectNo 변수를 사용하여 사용자가 선택한 메뉴 번호를 저장합니다.    */
-        Board board = null; /*  board 변수를 사용하여 게시글 정보를 저장합니다.   */
+        Scanner scanner = new Scanner(System.in);                                                                        /*  Scanner 클래스를 사용하여 키보드 입력을 받습니다. */
+        boolean run = true;                                                                                              /*  run 변수를 사용하여 프로그램이 실행 중인지 여부를 제어합니다.    */
+        int selectNo = 0;                                                                                               /*  selectNo 변수를 사용하여 사용자가 선택한 메뉴 번호를 저장합니다.    */
+        Board board = null;                                                                                             /*  board 변수를 사용하여 게시글 정보를 저장합니다.   */
 
-        while (run) {   /*  run 변수를 사용하여 프로그램이 실행 중인지 여부를 제어합니다.    */
-            System.out.println("-------------------------"); /*  구분선 출력 */
-            System.out.println("1.글작성 | 2.글조회 | 3.종료"); /*  메뉴 출력   */
-            System.out.println("-------------------------"); /*  구분선 출력 */
-            System.out.print("선택> ");                        /*     사용자 입력 받기  */
-            selectNo = scanner.nextInt();                   /*   사용자 입력을 selectNo 변수에 저장    */
-            if (selectNo == 1) {                            /*  사용자가 1번 메뉴를 선택한 경우  */
-                System.out.print("글번호: ");              /*  글번호 입력 받기   */
-                Long id = scanner.nextLong();             /*  글번호를 id 변수에 저장    */
-                System.out.print("제목: ");               /*  제목 입력 받기    */
-                String boardTitle = scanner.next();     /*  제목을 boardTitle 변수에 저장   */
-                System.out.print("작성자: ");              /*  작성자 입력 받기   */
-                String boardWriter = scanner.next();    /* 작성자를 boardWriter 변수에 저장 */
-                System.out.print("내용: ");               /*  내용 입력 받기    */
-                String boardContents = scanner.next();  /*  내용을 boardContents 변수에 저장    */
-                board = new Board(id, boardTitle, boardWriter, boardContents);  /*  Board 클래스의 생성자를 사용하여 게시글 정보를 저장합니다. */
-            } else if (selectNo == 2) {     /*  사용자가 2번 메뉴를 선택한 경우  */
-                System.out.print("조회할 글번호: ");  /*  조회할 글번호 입력 받기   */
-                Long id = scanner.nextLong();   /*  조회할 글번호를 id 변수에 저장  */
-                board.findById(id); /*  Board 클래스의 findById() 메서드를 사용하여 게시글 정보를 조회합니다.  */
-            } else if (selectNo == 3) { /*  사용자가 3번 메뉴를 선택한 경우  */
-                run = false;    /*  run 변수를 false로 설정하여 반복문을 종료합니다. */
+        while (run) {                                                                                                   /*  run 변수를 사용하여 프로그램이 실행 중인지 여부를 제어합니다.    */
+            System.out.println("-------------------------");                                                             /*  구분선 출력 */
+            System.out.println("1.글작성 | 2.글조회 | 3.종료");                                                              /*  메뉴 출력   */
+            System.out.println("-------------------------");                                                              /*  구분선 출력 */
+            System.out.print("선택> ");                                                                                   /*     사용자 입력 받기  */
+            selectNo = scanner.nextInt();                                                                                 /*   사용자 입력을 selectNo 변수에 저장    */
+            if (selectNo == 1) {                                                                                        /*  사용자가 1번 메뉴를 선택한 경우  */
+                System.out.print("글번호: ");                                                                            /*  글번호 입력 받기   */
+                Long id = scanner.nextLong();                                                                             /*  글번호를 id 변수에 저장    */
+                System.out.print("제목: ");                                                                                /*  제목 입력 받기    */
+                String boardTitle = scanner.next();                                                                      /*  제목을 boardTitle 변수에 저장   */
+                System.out.print("작성자: ");                                                                               /*  작성자 입력 받기   */
+                String boardWriter = scanner.next();                                                                      /* 작성자를 boardWriter 변수에 저장 */
+                System.out.print("내용: ");                                                                               /*  내용 입력 받기    */
+                String boardContents = scanner.next();                                                                   /*  내용을 boardContents 변수에 저장    */
+                board = new Board(id, boardTitle, boardWriter, boardContents);                                          /*  Board 클래스의 생성자를 사용하여 게시글 정보를 저장합니다. */
+            } else if (selectNo == 2) {                                                                                  /*  사용자가 2번 메뉴를 선택한 경우  */
+                System.out.print("조회할 글번호: ");                                                                        /*  조회할 글번호 입력 받기   */
+                Long id = scanner.nextLong();                                                                           /*  조회할 글번호를 id 변수에 저장  */
+                board.findById(id);                                                                                     /*  Board 클래스의 findById() 메서드를 사용하여 게시글 정보를 조회합니다.  */
+            } else if (selectNo == 3) {                                                                                 /*  사용자가 3번 메뉴를 선택한 경우  */
+                run = false;                                                                                            /*  run 변수를 false로 설정하여 반복문을 종료합니다. */
             }
         }
     }
