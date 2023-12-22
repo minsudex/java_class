c508cb851cd65551842aca0092778a16a296dd45
minsudex <minsuf4@naver.com>
2023-12-22 11:11:40 +0900

    수정 및 주석 추가

--- src/ch11_classes/ex06_memberboard/dto/BoardDTO.java
+++ src/ch11_classes/ex06_memberboard/dto/BoardDTO.java
@@ -1,15 +1,16 @@
-package ch11_classes.ex06_memberboard.dto;
+package ch11_classes.ex06_memberboard.dto;      //  package ch11_classes.ex06_memberboard.dto;: 이 클래스가 속한 패키지입니다.
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;                 //  import java.time.LocalDateTime;: 날짜와 시간을 다루기 위한 클래스를 불러옵니다.
+import java.time.format.DateTimeFormatter;      //  import java.time.format.DateTimeFormatter;: 날짜와 시간의 형식을 지정하기 위한 클래스를 불러옵니다.
 
-public class BoardDTO {
-    private Long id;
-    private String boardTitle;
-    private String boardWriter;
-    private String boardContents;
-    private int boardHits = 0;
-    private String createdAt;
+public class BoardDTO {                         //  public class BoardDTO {: 게시판 정보를 담는 데이터 전송 객체(DTO) 클래스를 정의합니다.
+
+    private Long id;                            //  private Long id;: 게시글의 고유한 식별 번호를 저장합니다.
+    private String boardTitle;                  //  private String boardTitle;: 게시글 제목을 저장합니다.
+    private String boardWriter;                 //  private String boardWriter;: 게시글 작성자를 저장합니다.
+    private String boardContents;               //  private String boardContents;: 게시글 내용을 저장합니다.
+    private int boardHits = 0;                  //  private int boardHits = 0;: 게시글 조회수를 저장하며, 기본값은 0입니다.
+    private String createdAt;                   //  private String createdAt;: 게시글 작성 시각을 저장합니다.
 
     public Long getId() {
         return id;
@@ -59,11 +60,11 @@ public class BoardDTO {
         this.createdAt = createdAt;
     }
 
-    public BoardDTO() {
+    public BoardDTO() {                             //  public BoardDTO() {: 빈 객체를 생성하는 기본 생성자입니다.
 
     }
 
-    private static Long idValue = 1L;
+    private static Long idValue = 1L;           //
 
     public BoardDTO(String boardTitle, String boardWriter, String boardContents) {
         this.id = idValue++;
